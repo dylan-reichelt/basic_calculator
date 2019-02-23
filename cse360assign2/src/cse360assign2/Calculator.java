3c04016c3dcc9f68d80c680c48464967efe0290d
dylan <dylan.reichelt1@gmail.com>
2019-02-22 20:56:26 -0700

    Initial Commit With javadoc format

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,67 @@
+/*
+ * Created by: Dylan Reichelt
+ * Student Code: 533
+ * Creates a calculator that can add, divide, subtract and multiply.
+ */
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator ()
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total of the calculator.
+	 * @return
+	 */
+	public int getTotal ()
+	{
+		return 0;
+	}
+	
+	/**
+	 * Adds a value to the total.
+	 * @param value
+	 */
+	public void add (int value)
+	{
+		
+	}
+	
+	/**
+	 * Subtracts a value from the total.
+	 * @param value
+	 */
+	public void subtract (int value)
+	{
+		
+	}
+	
+	/**
+	 * multiplies a value to total.
+	 * @param value
+	 */
+	public void multiply (int value)
+	{
+		
+	}
+	
+	/**
+	 * Divides a value from the total.
+	 * @param value
+	 */
+	public void divide (int value)
+	{
+		
+	}
+	
+	public String getHistory ()
+	{
+		return "";
+	}
+}
